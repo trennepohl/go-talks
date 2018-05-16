60fb86bb675e0ff7a138268c8689f8fcc8db1e46
Thiago Trennepohl <thiagotrennepohl@gmail.com>
2018-05-16 09:03:13 -0300

    Add a few slides, wip

---
+++ meetup/floripa-gophers/2018-05-18/demo.go
@@ -0,0 +1,24 @@
+// package main
+
+// import (
+// 	"html/template"
+// 	"os"
+// )
+
+// type Inventory struct {
+// 	Material string
+// 	Count    uint
+// }
+
+// func main() {
+// 	sweaters := Inventory{"wool", 17}
+// 	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	err = tmpl.Execute(os.Stdout, sweaters)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// }
