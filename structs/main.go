c44b20c13cd9abe53fe576daf6717c810eaa36e9
Thiago Trennepohl <thiagotrennepohl@gmail.com>
2018-06-09 12:49:16 -0300

    Add structs

---
+++ structs/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thiagotrennepohl/go-talks/structs/aluno"
+	"github.com/thiagotrennepohl/go-talks/structs/universidade"
+)
+
+func main() {
+	raffa := &aluno.Aluno{
+		Nome:  "Raffa",
+		Idade: 25,
+		Notas: []float32{1, 2, 3},
+		Curso: "Como desviar de balas",
+		Turma: 12,
+	}
+
+	situacao := universidade.SituacaoFinal(raffa)
+	fmt.Println(situacao)
+
+}
