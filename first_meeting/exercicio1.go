58d322a706ad85947071675957fafab231b419d9
Thiago Trennepohl <thiagotrennepohl@gmail.com>
2018-06-29 21:34:44 -0300

    Add exercises

---
+++ first_meeting/exercicio1.go
@@ -0,0 +1,17 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var numero int
+// 	fmt.Println("Ei usuario, digite um numero")
+// 	fmt.Scan(&numero)
+
+// 	fmt.Println(retornaSucessor(numero))
+// }
+
+// func retornaSucessor(numero int) int {
+// 	return numero + 1
+// }
